a686d7853fce7416eb18c7460574afab9dd649d8
darksynd <kevinjo47@yahoo.co.id>
2021-03-20 00:24:21 +0700

    Delete Code directory

--- Code/Gk kepake/test1.py
+++
@@ -1,7 +0,0 @@
-with open("CowrieLogAnalyzeResult " + str(date.today()) + ".csv", 'ab') as csvfile:
-                writer = csv.writer(csvfile)
-                if(i == 0):
-                    writer.writerow(["Time Stamp", "Action Type", "Session", "IP", "Command List", "List Mitre Attack"])
-                    writer.writerow([resultPerLine["timestamp"], resultPerLine["actionType"], resultPerLine["session"], resultPerLine["source_ip"], resultPerLine["command"], mitre])
-                else:
-                    writer.writerow([resultPerLine["timestamp"], resultPerLine["actionType"], resultPerLine["session"], resultPerLine["source_ip"], resultPerLine["command"], mitre])
